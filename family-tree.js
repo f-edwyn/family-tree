3da2baad01c4efffd7dfc29d8e62a28f8093334b
Edwyn Flores <edwynflores@gmail.com>
2021-01-14 21:27:35 -0500

    family size start

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,23 @@
 class FamilyTree {
-  constructor () {}
+  constructor (value) {
+    this.value = value;
+    this.children = [];
+    if(!this.value || typeof this.value !== 'string'){
+      throw('');
+    }
+  }
+  insert(child){
+    this.children.push(child);
+  }
+  familySize(){
+
+  }
+  findMember(){
+
+  }
+  log(){
+
+  }
 }
 
 module.exports = FamilyTree;
