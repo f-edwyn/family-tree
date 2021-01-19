67d961408cfbc7ee9a12d9b183b2831e3ce7f643
Edwyn Flores <edwynflores@gmail.com>
2021-01-19 11:55:02 -0500

    refactored solution

--- family-tree.js
+++ family-tree.js
@@ -2,22 +2,26 @@ class FamilyTree {
   constructor (value) {
     this.value = value;
     this.children = [];
+
     if(!this.value || typeof this.value !== 'string'){
       throw('');
     }
   }
+
   insert(child){
     child = new FamilyTree(child);
     this.children.push(child);
-
   }
+
   familySize(){
     let valueSize = 0;
+
     if(this.value){
       valueSize = 1;
     }
     return valueSize + this.children.length;
   }
+
   findMember(name){
     for(let index = 0; index < this.children.length; index++){
       if(this.children[index].value === name) return this.children[index];
@@ -26,6 +30,7 @@ class FamilyTree {
 
   log(){
     let returnedLog = '';
+
     if(this.value){
       returnedLog = `-- ${this.value}\n`;
      }
